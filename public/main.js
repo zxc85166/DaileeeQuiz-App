79b3dec7bfd9b3abc095c35417576165beb11ffb
zxc85166 <jimmya011@gmail.com>
2021-07-08 13:36:39 +0800

    部分翻譯

--- public/main.js
+++ public/main.js
@@ -11,73 +11,70 @@ const app = Vue.createApp({
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r3d1PVoA4oEzb?format=png&name=small",
-          question:
-            "1.다음 지문을 읽고 ㄱ-ㄴ-ㄷ 에 들어갈 단어가 올바르게 나열된 것을 고르시오.",
+          question: "1. 請閱讀下面文章 選擇正確羅列順序的 ㄅ-ㄆ-ㄇ 單字",
           answers: {
-            a: "행복 - 저기를 - 달빛",
-            b: "사랑 - 여기를 - 햇빛",
-            c: "믿음 - 저곳을 - 구름",
-            d: "희망 - 이곳을 - 별빛",
+            a: "幸福 - 那裡 - 月光",
+            b: "愛 - 這裡 - 陽光",
+            c: "信任 - 那地方 - 雲朵",
+            d: "希望 - 這地方 - 星光",
           },
           correctAnswer: "d",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r4aH0VkAEHWPy?format=png&name=small",
-          question: "2.다음 사진에 대한 설명 중 틀린 것을 고르시오.",
+          question: "2. 對下圖的說明請挑出錯誤的選項",
           answers: {
-            a: "크리스마스에 업로드된 사진이다.",
-            b: "멤버의 어린 시절 사진과 같이 업로드 된 사진이다.",
-            c: "게시글을 올린 멤버는 '조아'이다.",
-            d: "게시글에 '데일리'라는 단어가 들어가 있다.",
+            a: "聖誕節上傳的照片",
+            b: "和成員小時候的照片一起上傳的照片",
+            c: "上傳文章的成員是'Zoa'",
+            d: "文章中包含'Daileee'一詞",
           },
           correctAnswer: "c",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
-          question:
-            "3.위클리 데뷔일부터 100일째 되는 날은 무슨 요일인지 고르시오.",
-          answers: { a: "월요일", b: "수요일", c: "금요일", d: "일요일" },
+          question: "3. 請選出從Weeekly出道開始第100天是星期幾",
+          answers: { a: "週一", b: "週三", c: "週五", d: "週日" },
+          correctAnswer: "b",
+        },
+        {
+          srcimg:
+            "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
+          question: "4. 在1theK<WE CLEAR> EP.2中選出成功完成電影觀看任務的成員",
+          answers: { a: "Jihan", b: "申智阭", c: "Monday", d: "李在希" },
           correctAnswer: "b",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
           question:
-            "4. 1theK<WE CLEAR> EP.2에서 영화 관람 미션에 성공한 멤버를 고르시오.",
-          answers: { a: "지한", b: "신지윤", c: "먼데이", d: "이재희" },
-          correctAnswer: "b",
-        },
-        {
-          srcimg:
-            "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
-          question:
-            "5. 1theK<키워드 조작단>에 제시된'소은'의 키워드로 옳지 않은 것을 고르시오.",
-          answers: { a: "언니킬러", b: "보조개", c: "만능캐", d: "푹주기관차" },
+            "5. 1theK<關鍵詞操作團>中提示的'昭垠'的關鍵詞，請選擇不對的選項",
+          answers: { a: "歐尼killer", b: "酒窩", c: "萬能角", d: "暴走先驅者" },
           correctAnswer: "d",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
           question:
-            "6. 2020년 9월 17일 V LIVE<도전스케치 퀴즈!🖌(출제자:지유니,째희)>에서 '지윤'이 그림의 제시어를 고르시오.",
-          answers: { a: "소금", b: "아이디어", c: "얼룩말", d: "포크레인" },
+            "6. 2020年9月17日 V LIVE <挑戰素描問答!🖌(出題者:智阭尼,在希)>中請選擇'智阭'這幅畫的提示語",
+          answers: { a: "鹽", b: "創意", c: "斑馬", d: "挖掘機" },
           correctAnswer: "c",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
           question:
-            "7. 다음의 위클리 조합에 공통으로 포함되어 있는 멤버를 고르시오.",
-          answers: { a: "강토즈", b: "설탕즈", c: "박소은", d: "조아" },
+            "7. 請選擇下列Weeekly CP中同時包含的成員:1.강토즈 2.설탕즈 3.공사장 4.짠소금",
+          answers: { a: "李在希", b: "Jihan", c: "朴昭垠", d: "Zoa" },
           correctAnswer: "b",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
           question:
-            "8. 위클리의 긋네이버스<세계 가정의 날> 기념 자선 화보 중 단체 사진 속 풍선에 쓰여 있는 문구를 고르시오.",
+            "8. 請選擇Weeekly的Good NEIGHBORS<世界家庭日>紀念慈善畫報中團體照片中的在氣球上所寫的句子",
           answers: {
             a: "WE ARE WEEEKLY",
             b: "WE ARE FRIEND",
@@ -90,28 +87,27 @@ const app = Vue.createApp({
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
           question:
-            "9. 2020년 7월 2일 Mnet<엠카운다운> 'Tag Me(@Me)'데뷔 무대의 이름으로 올바른 것을 고르시오.",
-          answers: { a: "이수진", b: "지한", c: "조아", d: "먼데이" },
+            "9. 請選擇誰是 2020年7月2日 以Mnet<M Countdown> 'Tag Me(@Me)出道舞臺的結尾精靈",
+          answers: { a: "李受珍", b: "Jihan", c: "Zoa", d: "Monday" },
           correctAnswer: "a",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
           question:
-            "10. 1theK<이번 생은 처음> EP.2 에서'2층 침대'팀이 만난 돌물'라쿤'의 이름으로 올바른 것을 고르시오.",
-          answers: { a: "초코", b: "보리", c: "모카", d: "라떼" },
+            "10. 1theK<今生第一次> EP.2中'上層床'組遇到的動物'浣熊'的名字，請選擇正確的選項",
+          answers: { a: "Choco", b: "Bori", c: "Moca", d: "Latte" },
           correctAnswer: "d",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r4zDkVEAMiBuX?format=png&name=small",
-          question:
-            "11. 다음<weee:kloud> 장면 속 자막으로 들어갈 단어를 작성하시오.",
+          question: "11. 請選擇下列場景中的字幕中的單字",
           answers: {
-            a: "위클라우드",
-            b: "보조개",
-            c: "만능캐",
-            d: "푹주기관차",
+            a: "weee:kloud~",
+            b: "安妞~",
+            c: "WEEEKLY~",
+            d: "Daileee~",
           },
           correctAnswer: "a",
         },
@@ -119,15 +115,15 @@ const app = Vue.createApp({
           srcimg:
             "https://pbs.twimg.com/media/E5r5AdYVIAkWqxC?format=png&name=small",
           question:
-            "12. 다음<주간연습실> 장면 속 가위바위보 게임에서 이긴 멤버의 이름을 작성하시오.",
-          answers: { a: "이수진", b: "지한", c: "조아", d: "먼데이" },
+            "12. 請選擇下列<一週練習室> 場面中，猜拳遊戲中獲勝的成員的名字",
+          answers: { a: "李受珍", b: "Jihan", c: "朴昭垠", d: "李在希" },
           correctAnswer: "b",
         },
         {
           srcimg:
             "https://pbs.twimg.com/media/E5r5b0wVcAIM7sF?format=png&name=120x120",
           question:
-            "13. 다음은 각 멤버의 생년월일로, 빈 칸에 들어갈 숫자의 합을 구하시오. ",
+            "13. 以下是各成員的出生年月日，求空格數之和 李受珍: 0000년 00월 00일 Monday: 0000년 00월 00일 Jihan: 0000년 00월 00일 申智阭: 0000년 00월 00일 朴昭垠: 0000년 00월 00일 Zoa: 0000년 00월 00일 李在希: 0000년 00월 00일",
           answers: { a: "14056", b: "13182", c: "14176", d: "15716" },
           correctAnswer: "c",
         },

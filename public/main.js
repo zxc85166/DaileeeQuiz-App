7649119534c5224ac210e1d3e0b4b98be5238868
zxc85166 <jimmya011@gmail.com>
2021-07-26 11:45:45 +0800

    1.png

--- public/main.js
+++ public/main.js
@@ -9,8 +9,7 @@ const app = Vue.createApp({
 
       questions: [
         {
-          srcimg:
-            "https://pbs.twimg.com/media/E52mNyVVgAUFO7V?format=png&name=small",
+          srcimg: "../src/1.png",
           question: "1. 請閱讀下面文章 選擇正確羅列順序的 ㄅ-ㄆ-ㄇ 單字",
           answers: {
             a: "幸福 - 那裡 - 月光",
